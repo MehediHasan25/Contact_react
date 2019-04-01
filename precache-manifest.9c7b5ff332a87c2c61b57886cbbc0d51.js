31e5cbe1414a3d07bd02d84c5903894aad32b021
MehediHasan25 <ishanmehadi@gmail.com>
2019-04-01 10:52:44 +0600

    Updates

---
+++ precache-manifest.9c7b5ff332a87c2c61b57886cbbc0d51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bf29dc4e25729b6551c",
+    "url": "/Contact_react/static/js/runtime~main.2bf29dc4.js"
+  },
+  {
+    "revision": "610787aaaca3e5508fd7",
+    "url": "/Contact_react/static/js/main.610787aa.chunk.js"
+  },
+  {
+    "revision": "5dcea58b53f694100b28",
+    "url": "/Contact_react/static/js/2.5dcea58b.chunk.js"
+  },
+  {
+    "revision": "610787aaaca3e5508fd7",
+    "url": "/Contact_react/static/css/main.799bff35.chunk.css"
+  },
+  {
+    "revision": "5dcea58b53f694100b28",
+    "url": "/Contact_react/static/css/2.1802e439.chunk.css"
+  },
+  {
+    "revision": "c39d62c5f380dbae0fa46bafbffa96ed",
+    "url": "/Contact_react/index.html"
+  }
+];
